840e2061783dbbb1d2861146def3e30ba5e963e1
Michal Jura <mjura@suse.com>
2020-03-27 12:47:19 +0100

    [v1.18]: Add CaaSP kube-scheduler container image

---
+++ caasp-kube-scheduler-image/config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+ln -sf /usr/bin/kube-scheduler /usr/local/bin/kube-scheduler
+
+exit 0
