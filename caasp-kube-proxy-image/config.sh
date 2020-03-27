0cfeb933aec1d400bb1d9a408220e56a921ce0dc
Michal Jura <mjura@suse.com>
2020-03-27 13:09:43 +0100

    [v1.18]: Add CaaSP kube-proxy container image

---
+++ caasp-kube-proxy-image/config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+ln -sf /usr/bin/kube-proxy /usr/local/bin/kube-proxy
+
+exit 0
